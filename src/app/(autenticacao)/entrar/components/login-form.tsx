4e029c0273306b21292fcfad4bbdd3aea6ea68da
Thomas Henrique <thomas.santos02209@gmail.com>
2025-06-04 15:56:18 -0300

    refactor: adiciona imagem a pagina de login

--- src/app/(autenticacao)/entrar/components/login-form.tsx
+++ src/app/(autenticacao)/entrar/components/login-form.tsx
@@ -3,29 +3,34 @@
 import { GitHubIcon } from "@/components/icons/github";
 import { GoogleIcon } from "@/components/icons/google";
 import { Logo } from "@/components/shared/logo";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 
 export function LoginForm() {
-
-
   return (
     <form className="max-w-lg mx-auto w-full flex flex-col gap-4">
-      <div>
+      <div className="flex flex-col items-center mb-4">
+        <Logo className="text-7xl" />
         <h2 className="text-3xl text-center font-medium">
-          Bem vindo(a) ao <Logo />
+          Bem vindo(a) ao Sankofa.AI
         </h2>
         <span className="text-muted-foreground text-sm text-center">
           Por favor, entre em sua conta para ter acesso a plataforma.
         </span>
       </div>
 
-      <a href="http://localhost:5000/auth/google" className={buttonVariants({variant:"outline"})}>
-          <GoogleIcon />
-          Entrar com Google
+      <a
+        href="http://localhost:5000/auth/google"
+        className={buttonVariants({ variant: "outline" })}
+      >
+        <GoogleIcon />
+        Entrar com Google
       </a>
-      <a href="http://localhost:5000/auth/github" className={buttonVariants({variant:"outline"})}>
-          <GitHubIcon />
-          Entrar com GitHub
+      <a
+        href="http://localhost:5000/auth/github"
+        className={buttonVariants({ variant: "outline" })}
+      >
+        <GitHubIcon />
+        Entrar com GitHub
       </a>
     </form>
   );
--- src/app/(autenticacao)/entrar/page.tsx
+++ src/app/(autenticacao)/entrar/page.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import { LoginForm } from "./components/login-form";
 
 export default function Login() {
   return (
     <main className="w-full h-screen flex items-center justify-center">
-      <LoginForm />
+      <Image
+        src="/sankofa-art.jpg"
+        className="hidden md:block h-screen w-2/3 object-cover"
+        width={1920}
+        height={1280}
+        alt="Arte do movimento negro em vermelho"
+      />
+      <LoginForm /> 
     </main>
   );
 }
